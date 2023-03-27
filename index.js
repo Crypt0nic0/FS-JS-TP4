3f0e792f6fdc95f0e99539918322e50d5908829d
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-27 14:27:45 +0200

    TP Finished

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar
+// ranger la sidebar si on reclick dessus
+
+const btnToggle = document.querySelector('.toggle-btn');
+const sideBar = document.getElementById("side-bar");
+const content = document.querySelector('.content');
+
+btnToggle.addEventListener("click", () => {
+    sideBar.classList.toggle("sidebarToggle");
+    content.classList.toggle("content2");
+});
 
 // Ranger la sidebar si on click sur le contenu principal
+
+content.addEventListener("click", () => {
+    sideBar.classList.remove("sidebarToggle");
+    content.classList.remove("content2");
+});
 
 // BONUS
 // Créer en css l'hamburger menu qui se transforme en croix
